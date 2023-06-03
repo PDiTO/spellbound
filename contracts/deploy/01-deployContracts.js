8c61303a2db8e2e05dd396081cf13a5c8d4c9ba6
Paul Davis <pd@perfectdimension.com>
2023-06-03 12:31:04 +0900

    Setting up hardhat with deploy, adding test script.

---
+++ contracts/deploy/01-deployContracts.js
@@ -0,0 +1,24 @@
+const { deployments, getNamedAccounts } = require("hardhat");
+
+module.exports = async function main() {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with ${ethers.utils.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+};
+
+module.exports.tags = ["contracts"];
